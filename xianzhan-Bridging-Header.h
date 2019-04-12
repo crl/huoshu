da2085eb15941dad724e971bc21bb26d5cdfcf8a
crl <crlclr@gmail.com>
2019-04-12 15:49:53 +0800

    〜

--- xianzhan-Bridging-Header.h
+++ xianzhan-Bridging-Header.h
@@ -2,11 +2,11 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+//#import <ChannelSDK/ChannelSDK.h>
 #import <ChannelSDK/GameSDKFacade.h>
-
 #import <ChannelSDK/SDKNotifications.h>
 #import <ChannelSDK/SDKLoginAty.h>
-#import <ChannelSDK/GameSDKFacade.h>
+//#import <ChannelSDK/SettingVars.h>
 
 //#import <HuoShuSDK/HuoShuSDK.h>
 //#import <HuoShuSDK/HuoShuSDKMgr.h>
